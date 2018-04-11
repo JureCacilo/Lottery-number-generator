5ea94e72d6fd38019a4af51043a603bc9abbb090
Jure cacilo <cacilojurre@gmail.com>
2018-04-11 18:48:47 +0200

    GUI added

--- generator_loto_stevil.py
+++ generator_loto_stevil.py
@@ -1,21 +1,31 @@
+import tkinter
+from tkinter import messagebox
 from random import randint
 
-def lottery_number_generator(length):
+window = tkinter.Tk()
+
+def lottery_number_generator():
     loto_list = []
     while True:
         loto_number = randint(1, 50)
         if loto_number not in loto_list:
             loto_list.append(loto_number)
 
-        if len(loto_list) == length:
+        if len(loto_list) == int(input.get()):
             break
-    return loto_list
-
-def main():
-    print("Welcome to the Lottery numbers generator.")
-    number = int(input("Please enter how many random numbers would you like to have: "))
-    print(lottery_number_generator(number))
-
-main()
+    messagebox.showinfo("Loto numbers: ", loto_list)
 
 
+welcome = tkinter.Label(window,  text ="Welcome to the Lottery numbers generator.\n" +
+"Please enter how many random numbers would you like to have: ")
+welcome.pack()
+
+input = tkinter.Entry(window)
+input.pack()
+
+submit = tkinter.Button(window, text = "Submit", command = lottery_number_generator)
+submit.pack()
+
+
+
+window.mainloop()
